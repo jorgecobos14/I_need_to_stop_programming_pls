cfa284068b7ad06b4e28003c247753264b380cf3
Jorge <mariadolores2140@email.com>
2026-04-28 23:46:12 -0700

    app final

--- app/src/main/java/com/app/MainActivity.java
+++ app/src/main/java/com/app/MainActivity.java
@@ -16,8 +16,8 @@ import android.webkit.WebViewClient;
 public class MainActivity extends Activity {
 
     private WebView webView;
-    private static final String HOME_URL = "https://parameter-cod-idle-wto.trycloudflare.com";
-    private static final String ALLOWED_HOST = "parameter-cod-idle-wto.trycloudflare.com";
+    private static final String HOME_URL = "https://refers-advocacy-yards-beginner.trycloudflare.com";
+    private static final String ALLOWED_HOST = "refers-advocacy-yards-beginner.trycloudflare.com";
     private static final int REQ_MICROPHONE = 1001;
 
     private PermissionRequest pendingPermissionRequest;
