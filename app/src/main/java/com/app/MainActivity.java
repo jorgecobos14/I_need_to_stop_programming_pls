f87e3a6e0e7a28ac0ad02b37e09f47c58fdbabc3
Jorge <mariadolores2140@email.com>
2026-05-02 10:21:01 -0700

    app final

--- app/src/main/java/com/app/MainActivity.java
+++ app/src/main/java/com/app/MainActivity.java
@@ -30,8 +30,8 @@ public class MainActivity extends Activity {
     private ValueCallback<Uri[]> filePathCallback;
     private static final int REQ_FILE_CHOOSER = 1003;
 
-    private static final String HOME_URL = "https://auckland-stereo-region-led.trycloudflare.com";
-    private static final String ALLOWED_HOST = "auckland-stereo-region-led.trycloudflare.com";
+    private static final String HOME_URL = "https://sonic-costumes-eco-buf.trycloudflare.com";
+    private static final String ALLOWED_HOST = "sonic-costumes-eco-buf.trycloudflare.com";
 
     private static final int REQ_MICROPHONE = 1001;
     private static final int REQ_STORAGE    = 1002;
